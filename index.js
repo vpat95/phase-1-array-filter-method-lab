a4eb07bdd4bf8c99ac991501ac9b79afe0128273
Vivek Patel <vpatel360@gmail.com>
2023-06-27 10:46:01 -0400

    commit tree 786982911946c2f35efbee099fe3ce2267bd5d3b

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(array, entry){
+    return array.filter(driver => driver.toLowerCase() === entry.toLowerCase())
+}
+
+function fuzzyMatch(array, entry){
+    return array.filter(driver => driver.substring(0, entry.length) === entry)
+}
+
+function matchName(array, entry){
+    return array.filter(driver => driver.name === entry)
+}
